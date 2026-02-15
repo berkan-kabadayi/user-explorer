611535f3053abe7d9a3b3f3b460c4418cd7f7eb3
Berkan Kabadayı <kabadayiberkan@gmail.com>
2026-02-15 15:27:42 +0300

    feat: add localStorage persistence to favorites store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,29 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
 import type { FavoritesStore } from "../types/types";
 
-export const useFavoritesStore = create<FavoritesStore>()((set, get) => ({
-  favorites: [],
+export const useFavoritesStore = create<FavoritesStore>()(
+  persist(
+    (set, get) => ({
+      favorites: [],
 
-  addFavorite: (item) =>
-    set((state) => ({
-      favorites: [...state.favorites, item],
-    })),
+      addFavorite: (item) =>
+        set((state) => ({
+          favorites: [...state.favorites, item],
+        })),
 
-  removeFavorite: (id) =>
-    set((state) => ({
-      favorites: state.favorites.filter((fav) => fav.id !== id),
-    })),
+      removeFavorite: (id) =>
+        set((state) => ({
+          favorites: state.favorites.filter((fav) => fav.id !== id),
+        })),
 
-  isFavorite: (id) => {
-    return get().favorites.some((fav) => fav.id === id);
-  },
-}));
+      isFavorite: (id) => {
+        return get().favorites.some((fav) => fav.id === id);
+      },
+    }),
+    {
+      name: "favorites-storage",
+    }
+  )
+);
